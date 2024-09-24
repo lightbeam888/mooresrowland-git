dab6d40ac58b0dba9ec6feb7c2bbdabcb5c47f44
burhanuddinbaig <burhan.uddin.baig1@gmail.com>
2024-09-24 17:36:27 +0500

    updated allowed hosts

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 SECRET_KEY = "i#-m&d_^pita=e*(si)7#^*ys((tlcer&%*)j%07o!#guwoyk*"
 
 # Add your site's domain name(s) here.
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "mooresrowland.net", "http:/mooresrowland.net", "https:/mooresrowland.net"]
 
 # To send email from the server, we recommend django_sendmail_backend
 # Or specify your own email backend such as an SMTP server.
